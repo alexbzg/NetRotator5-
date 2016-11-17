bf5081cd5de8b4e059335a34f8373f39848c7cfc
alexbzg <alexbzg@gmail.com>
2016-11-17 17:05:51 +0300

    -

--- RotatorLauncher/FMain.Designer.cs
+++ RotatorLauncher/FMain.Designer.cs
@@ -28,13 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FMain));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.ddbSettings = new System.Windows.Forms.ToolStripDropDownButton();
             this.miPathOld = new System.Windows.Forms.ToolStripMenuItem();
             this.miPath5 = new System.Windows.Forms.ToolStripMenuItem();
             this.ofdRotator = new System.Windows.Forms.OpenFileDialog();
+            this.dgvDevices = new System.Windows.Forms.DataGridView();
+            this.Band = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Run = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Run2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmDevices = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDevices)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -62,14 +69,14 @@
             // miPathOld
             // 
             this.miPathOld.Name = "miPathOld";
-            this.miPathOld.Size = new System.Drawing.Size(152, 22);
+            this.miPathOld.Size = new System.Drawing.Size(146, 22);
             this.miPathOld.Text = "NetRotator";
             this.miPathOld.Click += new System.EventHandler(this.miPathClick);
             // 
             // miPath5
             // 
             this.miPath5.Name = "miPath5";
-            this.miPath5.Size = new System.Drawing.Size(152, 22);
+            this.miPath5.Size = new System.Drawing.Size(146, 22);
             this.miPath5.Text = "NetRotator5+";
             this.miPath5.Click += new System.EventHandler(this.miPathClick);
             // 
@@ -79,16 +86,62 @@
             this.ofdRotator.Filter = "AntennaNetRotator.exe|AntennaNetRotator.exe";
             this.ofdRotator.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdRotator_FileOk);
             // 
+            // dgvDevices
+            // 
+            this.dgvDevices.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDevices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDevices.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Band,
+            this.Run,
+            this.Run2});
+            this.dgvDevices.ContextMenuStrip = this.cmDevices;
+            this.dgvDevices.Location = new System.Drawing.Point(0, 0);
+            this.dgvDevices.Name = "dgvDevices";
+            this.dgvDevices.Size = new System.Drawing.Size(355, 312);
+            this.dgvDevices.TabIndex = 1;
+            this.dgvDevices.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDevices_CellClick);
+            // 
+            // Band
+            // 
+            this.Band.DataPropertyName = "band";
+            this.Band.Frozen = true;
+            this.Band.HeaderText = "Band";
+            this.Band.Name = "Band";
+            this.Band.ReadOnly = true;
+            // 
+            // Run
+            // 
+            this.Run.DataPropertyName = "run";
+            this.Run.HeaderText = "Run";
+            this.Run.Name = "Run";
+            this.Run.ReadOnly = true;
+            // 
+            // Run2
+            // 
+            this.Run2.DataPropertyName = "run2";
+            this.Run2.HeaderText = "2nd";
+            this.Run2.Name = "Run2";
+            this.Run2.ReadOnly = true;
+            // 
+            // cmDevices
+            // 
+            this.cmDevices.Name = "cmDevices";
+            this.cmDevices.Size = new System.Drawing.Size(153, 26);
+            // 
             // FMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(355, 314);
+            this.Controls.Add(this.dgvDevices);
             this.Controls.Add(this.toolStrip1);
             this.Name = "FMain";
             this.Text = "Form1";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDevices)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +154,11 @@
         private System.Windows.Forms.ToolStripMenuItem miPathOld;
         private System.Windows.Forms.ToolStripMenuItem miPath5;
         private System.Windows.Forms.OpenFileDialog ofdRotator;
+        private System.Windows.Forms.DataGridView dgvDevices;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Band;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Run;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Run2;
+        private System.Windows.Forms.ContextMenuStrip cmDevices;
     }
 }
 
--- RotatorLauncher/FMain.cs
+++ RotatorLauncher/FMain.cs
@@ -4,22 +4,81 @@ using System.ComponentModel;
 using System.IO;
 using System.Windows.Forms;
 using System.Xml.Serialization;
+using System.Diagnostics;
 
 namespace RotatorLauncher
 {
     public partial class FMain : Form
     {
         static readonly string[] exeNames = { "AntennaNetRotator.exe", "AntennaNetRotator5.exe" };
+        static readonly string[] bands = { "10", "15", "20", "40", "80", "160" };
+
+        private class DeviceEntry
+        {
+            int programType;
+            string name;
+            int index;
+
+
+        };
+
+        private class DeviceRow
+        {
+            string _Band;
+            string _Run;
+            string _Run2;
+
+            public string band
+            {
+                get { return _Band; }
+                set { _Band = value; }
+            }
+
+            public string run
+            {
+                get { return _Run; }
+                set { _Run = value; }
+            }
+
+            public string run2
+            {
+                get { return _Run2; }
+                set { _Run2 = value; }
+            }
+
+
+        };
+
         FormState[] rotatorStates = new FormState[] { null, null };
         AppConf appConf = new AppConf();
         int ofdIndex = -1;
+        BindingList<DeviceRow> blDevices = new BindingList<DeviceRow>();
+        BindingSource bsDevices;
 
         public FMain()
         {
             InitializeComponent();
             readConfig();
+
             for (int co = 0; co < 2; co++)
+            {
                 setRotatorPath(co, appConf.rotatorPaths[co]);
+                if ( rotatorStates[co] != null )
+                {
+                    ToolStripMenuItem mi = new ToolStripMenuItem();
+                    mi.Text = ddbSettings.DropDownItems[co].Text;
+                    cmDevices.Items.Add(mi);
+                }
+            }
+
+            bsDevices = new BindingSource(blDevices, null);
+            dgvDevices.AutoGenerateColumns = false;
+            dgvDevices.DataSource = bsDevices;
+
+            foreach ( string band in bands)
+            {
+                blDevices.Add(new DeviceRow { band = band });
+            }
         }
 
         private FormState loadRotatorConfig(string path)
@@ -92,7 +151,10 @@ namespace RotatorLauncher
             }
         }
 
-
+        private void dgvDevices_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Debug.WriteLine("cell click");
+        }
     }
 
     public class ConnectionSettings
